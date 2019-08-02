f0e95ed580d381bb8955cc6242d9495c1296b703
Runar Bjarnason <runarorama@gmail.com>
2019-08-02 14:32:44 -0400

    wrong sha again

--- unison-language.rb
+++ unison-language.rb
@@ -6,7 +6,7 @@ class UnisonLanguage < Formula
 
   if OS.mac?
     url "https://github.com/unisonweb/unison/releases/download/release%2FM1a/unison-osx.tar.gz"
-    sha256 "D54A5E79AFB35DD506B204CB28563B2779DE120625D0C12EC8EFB64AB35A26DD"
+    sha256 "a1fd7210b02a4d389bb89d39538767b70cb5ddb1af5045b876c315fa87d1e6db"
   elsif OS.linux?
     url "https://github.com/unisonweb/unison/releases/download/release%2FM1a/unison-linux64.tar.gz"
     sha256 "4ba52595d2d89772327c689f08728e5f19499f3181b712b8708e71da2fccf4e6"
