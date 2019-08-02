18e3d78c4e89a20a2935e726e6f7ac989e63f19f
Runar Bjarnason <runarorama@gmail.com>
2019-08-02 14:30:25 -0400

    wrong hash

--- unison-language.rb
+++ unison-language.rb
@@ -6,7 +6,7 @@ class UnisonLanguage < Formula
 
   if OS.mac?
     url "https://github.com/unisonweb/unison/releases/download/release%2FM1a/unison-linux64.tar.gz"
-    sha256 "C358A3DF81FE24CFDC2B6D9E035D642A9EE52464C9AD226EA7387B913F26B2B7"
+    sha256 "4ba52595d2d89772327c689f08728e5f19499f3181b712b8708e71da2fccf4e6"
   elsif OS.linux?
     url "https://github.com/unisonweb/unison/releases/download/release%2FM1a/unison-osx.tar.gz"
     sha256 "D54A5E79AFB35DD506B204CB28563B2779DE120625D0C12EC8EFB64AB35A26DD"
