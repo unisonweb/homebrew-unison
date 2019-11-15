683c37ea538f8a110804260a3df2f053df2ab2b9
azure pipeline <azure@azure.com>
2019-11-15 07:06:06 +0000

    update to release/M1f

--- unison-language.rb
+++ unison-language.rb
@@ -2,14 +2,14 @@ class UnisonLanguage < Formula
   desc "The Unison Language and Codebase Manager."
   homepage "https://unisonweb.org"
 
-  version "1.0.M1e"
+  version "1.0.M1f"
 
   if OS.mac?
-    url "https://github.com/unisonweb/unison/releases/download/release%2FM1e/unison-osx.tar.gz"
-    sha256 "d0b02776217de5fd90ab1d2599e5c1ed97f8ef65a97a72c231795b991b6292c9"
+    url "https://github.com/unisonweb/unison/releases/download/release%2FM1f/unison-osx.tar.gz"
+    sha256 "d3d51e2ebc1faabb164ef33b0635ef459b6d54527c6a9d865e4dc39716583621"
   elsif OS.linux?
-    url "https://github.com/unisonweb/unison/releases/download/release%2FM1e/unison-linux64.tar.gz"
-    sha256 "a17770f9abf537124bea36f162351ec643716e0eef2b843b1c1bd37450ef06cb"
+    url "https://github.com/unisonweb/unison/releases/download/release%2FM1f/unison-linux64.tar.gz"
+    sha256 "319b882b38ff210ce4cc753a82439e7c3ed770739fc209422a8153abae66024b"
   end
 
   def install
