809de75c07d6ad3bc053b1718b176815d563331f
Paul Chiusano <paul.chiusano@gmail.com>
2021-08-24 21:07:02 -0500

    M2h release

--- unison-language.rb
+++ unison-language.rb
@@ -3,15 +3,15 @@ class UnisonLanguage < Formula
   homepage "https://unisonweb.org"
   license "MIT"
 
-  version "1.0.M2g"
+  version "1.0.M2h"
   if OS.mac?
-    url "https://github.com/unisonweb/unison/releases/download/release%2FM2g/ucm-macos.tar.gz"
-    sha256 "eae914c75e7c35839ab770bdbfe96d9eda204cf5a9f2ebca88eb5f977fbb69c5"
+    url "https://github.com/unisonweb/unison/releases/download/release%2FM2h/ucm-macos.tar.gz"
+    sha256 "12e0530262786648c7fffad01bdb12a682c0a36717cab06768d0457a388d07e8"
 
     head "https://github.com/unisonweb/unison/releases/download/latest-macOS/unison-macOS.tar.gz"
   elsif OS.linux?
-    url "https://github.com/unisonweb/unison/releases/download/release%2FM2g/ucm-linux.tar.gz"
-    sha256 "d2a67dd2a4997e79aea8c95eeae412f4b28eee74944c1a7766b39e62f0e99200"
+    url "https://github.com/unisonweb/unison/releases/download/release%2FM2h/ucm-linux.tar.gz"
+    sha256 "b03f00be06f6b70219017f7a8f990bf8290975a66e57f8dc7209e23c5f2e2363"
 
     head "https://github.com/unisonweb/unison/releases/download/latest-Linux/unison-Linux.tar.gz"
   end
