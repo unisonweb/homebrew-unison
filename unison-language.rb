6745719200400f48e885858781829db44560f03f
Unison Bot 🤖 <Unison@unison.cloud>
2023-08-22 18:20:20 +0000

    release/M5e

--- unison-language.rb
+++ unison-language.rb
@@ -4,15 +4,15 @@ class UnisonLanguage < Formula
   license "MIT"
   depends_on "fzf" => :recommended
 
-  version "1.0.M5d"
+  version "1.0.M5e"
   if OS.mac?
-    url "https://github.com/unisonweb/unison/releases/download/release%2FM5d/ucm-macos.tar.gz"
-    sha256 "3f82321809621ec35fdf737abfe49d4ab120bf81ebd9efe937f953f817001179"
+    url "https://github.com/unisonweb/unison/releases/download/release%2FM5e/ucm-macos.tar.gz"
+    sha256 "8e0f3f0e6209aeed8e299bb959f03b7dab4a71bbab3e25e7a002e27f12f6ea47"
 
     head "https://github.com/unisonweb/unison/releases/download/latest/ucm-macos.tar.gz"
   elsif OS.linux?
-    url "https://github.com/unisonweb/unison/releases/download/release%2FM5d/ucm-linux.tar.gz"
-    sha256 "b239d3ccf0d3923f1cb7a2623ab43fc1e7bdc8695bf57a950ec5d75c03ed66a0"
+    url "https://github.com/unisonweb/unison/releases/download/release%2FM5e/ucm-linux.tar.gz"
+    sha256 "fb6748c6a7fd6fc0dfa1351a93103a42ba5bedc010282bde9edc434b5b05c633"
 
     head "https://github.com/unisonweb/unison/releases/download/latest/ucm-linux.tar.gz"
   end
