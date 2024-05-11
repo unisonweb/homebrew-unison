ac5ffebc2127d27f72c91b8361f3973aa6be1905
Arya Irani <arya.irani@gmail.com>
2024-05-11 01:14:55 -0400

    skip racket install by default

--- unison-language.rb
+++ unison-language.rb
@@ -3,15 +3,16 @@ class UnisonLanguage < Formula
   homepage "https://unisonweb.org"
   license "MIT"
 
-  revision 1
+  revision 2
   version_scheme 1
 
   head do
     url "https://github.com/unisonweb/unison/releases/download/trunk-build/ucm-macos.tar.gz" if OS.mac?
     url "https://github.com/unisonweb/unison/releases/download/trunk-build/ucm-linux.tar.gz" if OS.linux?
-    depends_on "minimal-racket" => :recommended
   end
 
+  option "with-compile-native", "experimental support for `compile.native` (requires --HEAD)"
+  depends_on "minimal-racket" if build.with? "compile-native"
   depends_on "fzf" => :recommended
 
   if OS.mac?
@@ -27,24 +28,39 @@ class UnisonLanguage < Formula
       libexec.install "unison/unison"
       libexec.install "runtime"
       pkgshare.install "ui"
-      pkgshare.install "racket"
-      ENV["PLTUSERHOME"] = pkgshare/"racket"/"home"
-      ohai "Installing Racket unison library"
-      system "raco", "pkg", "install", "--scope", "user", "--auto", "#{pkgshare}/racket/unison.zip"
-      ohai "Installing Racket compiler-lib library"
-      system "raco", "pkg", "install", "--scope", "user", "--auto", "--skip-installed", "compiler-lib"
 
-      (bin/"ucm").write <<~EOS
-        #!/bin/bash
+      if build.with? "compile-native"
+        pkgshare.install "racket"
+        ENV["PLTUSERHOME"] = pkgshare/"racket"/"home"
+        ohai "Installing Racket unison library"
+        ohai "#{pkgshare}/racket/unison.zip"
+        system "raco", "pkg", "install", "--scope", "user", "--auto", "#{pkgshare}/racket/unison.zip"
+        ohai "Installing Racket compiler-lib library"
+        system "raco", "pkg", "install", "--scope", "user", "--auto", "--skip-installed", "compiler-lib"
 
-        export PLTUSERHOME="#{pkgshare}/racket/home"
-        echo "PLTUSERHOME=$PLTUSERHOME"
-        UCM_WEB_UI="#{opt_share}/ui" \\
-          #{opt_libexec}/unison \\
-            --runtime-path #{opt_libexec}/runtime/bin/unison-runtime \\
-            "$@"
-      EOS
+        (bin/"ucm").write <<~EOS
+          #!/bin/bash
+
+          export PLTUSERHOME="#{pkgshare}/racket/home"
+          UCM_WEB_UI="#{opt_share}/ui" \\
+            #{opt_libexec}/unison \\
+              --runtime-path #{opt_libexec}/runtime/bin/unison-runtime \\
+              "$@"
+        EOS
+      else
+        (bin/"ucm").write <<~EOS
+          #!/bin/bash
+
+          # This installation is not set up for `compile.native`.
+
+          UCM_WEB_UI="#{opt_share}/ui" \\
+            #{opt_libexec}/unison \\
+              --runtime-path #{opt_libexec}/runtime/bin/unison-runtime \\
+              "$@"
+        EOS
+      end
     else
+      odie "Option `--with-compile-native` requires --HEAD" if build.with? "compile-native"
       prefix.install Dir["*"]
       bin.install_symlink prefix/"ucm"
     end
@@ -52,7 +68,8 @@ class UnisonLanguage < Formula
 
   test do
     if build.head?
-      (testpath/"helloworld.md").write <<~EOS
+      ohai "Setting up project and downloading base..."
+      (testpath/"setup.md").write <<~EOS
         ```ucm
         .> project.create proj
         ```
@@ -60,16 +77,32 @@ class UnisonLanguage < Formula
         helloWorld = do printLine "Hello, World!"
         ```
         ```ucm
+        proj/main> add
+        ```
+      EOS
+      system "ucm", "transcript", "--save-codebase-to", testpath/"codebase", testpath/"setup.md"
+
+      ohai "Running helloWorld..."
+      (testpath/"run.md").write <<~EOS
+        ```ucm
         proj/main> run helloWorld
         ```
         ```ucm
         proj/main> run.native helloWorld
         ```
-        ```ucm
-        proj/main> compile.native helloWorld #{testpath/"helloWorld"}
-        ```
       EOS
-      system "ucm", "transcript", testpath/"helloworld.md"
+      system "ucm", "transcript.fork", "--codebase", testpath/"codebase", testpath/"run.md"
+
+      if build.with? "compile-native"
+        ohai "Compiling helloWorld..."
+        (testpath/"compile.md").write <<~EOS
+          ```ucm
+          proj/main> compile.native helloWorld #{testpath/"helloWorld"}
+          ```
+        EOS
+        system "ucm", "transcript.fork", "--codebase", testpath/"codebase", testpath/"compile.md"
+        system testpath/"helloWorld"
+      end
     else
       (testpath/"getbase.md").write <<~EOS
         ```ucm
